6b0cd6fbaa48dd9ba59c99784eec6a3bd2a60cbd
Ramzan <rdemistigov@gmail.com>
2022-09-15 18:10:57 +0300

    added folder for backend

---
+++ booking/src/App.js
@@ -0,0 +1,21 @@
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import HotelList from "./pages/HotelList/HotelList";
+import Hotel from "./pages/Hotel/Hotel";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotelList" element={<HotelList />} />
+          <Route path="/hotelList/:id" element={<Hotel />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
