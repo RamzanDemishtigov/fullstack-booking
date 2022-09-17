649e1fcea908596a00861eb081cd06b158a612f3
Magomed <93042393+demishtigov@users.noreply.github.com>
2022-09-17 17:39:09 +0300

    styled navbar

--- booking/src/components/navbar/Navbar.jsx
+++ booking/src/components/navbar/Navbar.jsx
@@ -3,7 +3,17 @@ import './navbar.scss'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <div className="navbar">
+         <div className="navbar__head">
+          <span className='navbar__logo'>Booking</span>
+          <div className="navbar__buttons">
+            <button className='navbar__button'>Register</button>
+            <button className='navbar__button'>Login</button>
+          </div>
+         </div>
+      </div>
+    </>
   )
 }
 
